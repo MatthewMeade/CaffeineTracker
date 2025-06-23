7e7f836d820cee0327574a7bfae4e91fc4da1e60
Matthew Meade <email@matthewmeade.ca>
2025-06-23 19:37:45 -0230

    feat: disable @typescript-eslint/unbound-method rule for test files

--- eslint.config.js
+++ eslint.config.js
@@ -43,6 +43,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-call": "off",
       "@typescript-eslint/no-unsafe-argument": "off",
       "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/unbound-method": "off",
     },
   },
   {
